5e55498133067068e7ad20563b13e4c53f8e0cd4
dreamingechoes <hi@dreamingecho.es>
2019-07-31 21:16:23 +0200

    Add new dependencies

--- final-projects/shop_api/mix.exs
+++ final-projects/shop_api/mix.exs
@@ -22,8 +22,17 @@ defmodule ShopApi.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # EctoEnum reference: https://hexdocs.pm/ecto_enum
+      {:ecto_enum, "~> 1.3"},
+
+      # EctoSQL reference: https://hexdocs.pm/ecto_sql
+      {:ecto_sql, "~> 3.1"},
+
+      # Plug.Cowboy reference: https://hexdocs.pm/plug_cowboy
+      {:plug_cowboy, "~> 2.0"},
+
+      # Postgrex reference: https://hexdocs.pm/postgrex
+      {:postgrex, "~> 0.15"}
     ]
   end
 end
