0fa283a8d4a8884edd54c8e16d788e2ca65e4592
dreamingechoes <hi@dreamingecho.es>
2019-08-07 18:24:46 +0200

    Remove dependency comments in mix.exs

--- final-projects/shop_api/mix.exs
+++ final-projects/shop_api/mix.exs
@@ -22,19 +22,10 @@ defmodule ShopApi.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # EctoEnum reference: https://hexdocs.pm/ecto_enum
       {:ecto_enum, "~> 1.3"},
-
-      # EctoSQL reference: https://hexdocs.pm/ecto_sql
       {:ecto_sql, "~> 3.1"},
-
-      # Plug.Cowboy reference: https://hexdocs.pm/plug_cowboy
       {:plug_cowboy, "~> 2.0"},
-
-      # Postgrex reference: https://hexdocs.pm/postgrex
       {:postgrex, "~> 0.15"},
-
-      # Jason reference: https://hexdocs.pm/jason
       {:jason, "~> 1.1"}
     ]
   end
